1eb06bfd7ea146cfe4ce77cae2f00ba16af486db
Verona01 <k1510@bk.ru>
2016-10-29 16:04:00 +0300

    Add files via upload

---
+++ HW3.js
@@ -0,0 +1,28 @@
+//дз2 №3 *
+
+/*var i, b;
+for (var i = 1; i < 100; i++) {
+	var b = '';
+
+if (i%3==0) {
+	b += 'Fizz';
+}
+
+if (i%5==0) {
+	b += 'Buzz';
+}
+if (b=='') b = i;
+console.log(b);
+}*/
+
+//дз3 #1
+
+/*var tr="#";
+while (tr.length<=7){
+
+    console.log(tr + "\n");
+
+    tr+="#";
+
+}*/
+
